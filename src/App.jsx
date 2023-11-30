721e4e5d68bb9845889e2c5762322e0bbacca3f4
noufel-boulkroune <noufelboulkroune@gmail.com>
2023-11-30 20:21:18 +0100

    Make the content and the styling dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import componentsImg from "./assets/components.png";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 
 // function CoreConcept(props) {
 //   return (
@@ -18,10 +18,24 @@ import { CORE_CONCEPTS } from "./data";
 //OR
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button!");
+  const [selectedTopic, setSelectedTopic] = useState(null);
 
   function handelSelect(selectedButton) {
     setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
   return (
     <div>
@@ -30,27 +44,91 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept
+                key={conceptItem.title}
+                title={conceptItem.title}
+                description={conceptItem.description}
+                image={conceptItem.image}
+              />
+            ))}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handelSelect("components")}>
+            <TabButton
+              onSelect={() => handelSelect("components")}
+              isSelected={selectedTopic === "components"}
+            >
               Components
             </TabButton>
-            <TabButton onSelect={() => handelSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handelSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handelSelect("state")}>State</TabButton>
+            <TabButton
+              onSelect={() => handelSelect("jsx")}
+              isSelected={selectedTopic === "jsx"}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              onSelect={() => handelSelect("props")}
+              isSelected={selectedTopic === "props"}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              onSelect={() => handelSelect("state")}
+              isSelected={selectedTopic === "state"}
+            >
+              State
+            </TabButton>
           </menu>
-          {selectedTopic}
+          {tabContent}
+          <p></p>
+          OR
+          <menu>
+            <TabButton
+              onSelect={() => handelSelect("components")}
+              isSelected={selectedTopic === "components"}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              onSelect={() => handelSelect("jsx")}
+              isSelected={selectedTopic === "jsx"}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              onSelect={() => handelSelect("props")}
+              isSelected={selectedTopic === "props"}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              onSelect={() => handelSelect("state")}
+              isSelected={selectedTopic === "state"}
+            >
+              State
+            </TabButton>
+          </menu>
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
